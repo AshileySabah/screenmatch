7c3c6cca3033062d7cd72941603bfc94e06be3fb
AshileySabah <ashileysabrina2002@gmail.com>
2024-09-20 21:05:27 -0300

    feat: interface Classificavel

---
+++ src/br/com/alura/screenmatch/modelos/Episodio.java
@@ -0,0 +1,58 @@
+package br.com.alura.screenmatch.modelos;
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Episodio implements Classificavel {
+    private int numero;
+    private String nome;
+    private Serie serie;
+    private String sinopse;
+    private int totalVisualizacoes;
+    
+    public int getTotalVisualizacoes() {
+        return totalVisualizacoes;
+    }
+
+    public void setTotalVisualizacoes(int totalVisualizacoes) {
+        this.totalVisualizacoes = totalVisualizacoes;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    @Override
+    public int getClassificacao() {
+        if(totalVisualizacoes > 100){
+            return 4;
+        }
+        return 2;
+    }
+}
