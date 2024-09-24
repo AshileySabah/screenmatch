90579e54d6da3a5ac7c3ccd933bc83209872fd5a
AshileySabah <ashileysabrina2002@gmail.com>
2024-09-23 21:01:29 -0300

    feat: sort Titulo

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch.principal;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
@@ -11,17 +13,27 @@ public class PrincipalComListas {
         Filme filme2 = new Filme("Em busca da Felicidade", 2004);
         Serie serie1 = new Serie("Stranger Things", 2019);
 
-        ArrayList<Titulo> lista = new ArrayList<Titulo>();
-        lista.add(filme1);
-        lista.add(filme2);
-        lista.add(serie1);
+        ArrayList<Titulo> filmes = new ArrayList<Titulo>();
+        filmes.add(filme1);
+        filmes.add(filme2);
+        filmes.add(serie1);
 
-        for (Titulo titulo : lista) {
+        for (Titulo titulo : filmes) {
             if(titulo instanceof Filme filme) {
-                System.out.println(filme.getClassificacao());
+                System.out.println("Classificação: " + filme.getClassificacao() + " - " + filme.getNome());
             }
-
             System.out.println(titulo);
         }
+
+        ArrayList<String> atores = new ArrayList<String>();
+        atores.add("Sandra Bullock");
+        atores.add("Anne Hathaway");
+        atores.add("Bruno Gagliasso");
+        System.out.println(atores);
+        Collections.sort(atores);
+        System.out.println(atores);
+
+        Collections.sort(filmes);
+        System.out.println(filmes);
     }
 }
